79acf39ddff9651ba49a85a5cd5228b0d7df8075
John Myles White <jmw@johnmyleswhite.com>
2010-08-24 13:59:53 -0400

    Fixed bug; default code now works

--- R/create.project.R
+++ R/create.project.R
@@ -18,12 +18,12 @@ function(project.name)
   
   file.create(file.path(project.name, 'lib', 'preprocess_data.R'))
   
-  file.copy(system.file('examples/load_data.R', package = 'ProjectTemplate'),
+  file.copy(system.file(file.path('defaults', 'boot.R'), package = 'ProjectTemplate'),
             file.path(project.name, 'lib', 'boot.R'))
-  file.copy(system.file('examples/load_data.R', package = 'ProjectTemplate'),
+  file.copy(system.file(file.path('defaults', 'load_data.R'), package = 'ProjectTemplate'),
             file.path(project.name, 'lib', 'load_data.R'))
-  file.copy(system.file('examples/load_data.R', package = 'ProjectTemplate'),
+  file.copy(system.file(file.path('defaults', 'load_libraries.R'), package = 'ProjectTemplate'),
             file.path(project.name, 'lib', 'load_libraries.R'))
-  file.copy(system.file('examples/load_data.R', package = 'ProjectTemplate'),
+  file.copy(system.file(file.path('defaults', 'utilities.R'), package = 'ProjectTemplate'),
             file.path(project.name, 'lib', 'utilities.R'))
 }
