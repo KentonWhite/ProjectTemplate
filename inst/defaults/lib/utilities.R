b12d3f33b0e4bc72b1511aba24c97b2383125c4b
Diego Valle-Jones <diegovalle@gmail.com>
2010-11-11 20:21:50 -0600

    uses make.names to create syntactically valid variable names
    The clean.variable.name function generates synctactilly invalid
    names if a file's name consists of only numbers, e.g '2006.csv'.
    This version uses the R function make.names from the base
    package to generate the correct variable names

--- inst/defaults/lib/utilities.R
+++ inst/defaults/lib/utilities.R
@@ -1,7 +1,5 @@
 clean.variable.name <- function(variable.name)
 {
-  variable.name <- gsub('_', '.', variable.name, perl = TRUE)
-  variable.name <- gsub('-', '.', variable.name, perl = TRUE)
-  variable.name <- gsub('\\s+', '.', variable.name, perl = TRUE)
+  variable.name <- make.names(variable.name)
   return(variable.name)
 }
