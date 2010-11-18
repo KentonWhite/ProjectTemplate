e9ace504fbffe5f855e9fd9fdab19105ecf19263
John Myles White <jmw@johnmyleswhite.com>
2010-11-17 19:29:53 -0500

    Fixed a bug in the name conversion function.

--- inst/defaults/lib/utilities.R
+++ inst/defaults/lib/utilities.R
@@ -1,7 +1,16 @@
 clean.variable.name <- function(variable.name)
 {
-  variable.name <- gsub('_', '.', variable.name, perl = TRUE)
-  variable.name <- gsub('-', '.', variable.name, perl = TRUE)
+  variable.name <- gsub('^[^a-zA-Z0-9]+', '', variable.name, perl = TRUE)
+  variable.name <- gsub('[^a-zA-Z0-9]+$', '', variable.name, perl = TRUE)
+  variable.name <- gsub('_+', '.', variable.name, perl = TRUE)
+  variable.name <- gsub('-+', '.', variable.name, perl = TRUE)
   variable.name <- gsub('\\s+', '.', variable.name, perl = TRUE)
+  variable.name <- gsub('\\.+', '.', variable.name, perl = TRUE)
+  variable.name <- make.names(variable.name)
   return(variable.name)
+}
+
+cache.name <- function(data.filename)
+{
+  return(gsub('\\..*', '', data.filename, perl = TRUE))
 }
