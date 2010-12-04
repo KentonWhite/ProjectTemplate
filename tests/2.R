62d8a1468c72a1c5230a816844a0fd182c01793c
John Myles White <jmw@johnmyleswhite.com>
2010-12-03 19:05:15 -0500

    Draft of v0.2-1 ready for testing

--- tests/2.R
+++ tests/2.R
@@ -1,9 +1,19 @@
 library('testthat')
 
-source('inst/defaults/lib/utilities.R')
+library('ProjectTemplate')
+
+create.project('test_project')
+
+setwd('test_project')
+
+load.project()
 
 expect_that(clean.variable.name('test_me'), equals('test.me'))
 expect_that(clean.variable.name('test-me'), equals('test.me'))
 expect_that(clean.variable.name('test..me'), equals('test.me'))
 expect_that(clean.variable.name('test me'), equals('test.me'))
 expect_that(clean.variable.name('1990'), equals('X1990'))
+
+setwd('..')
+
+unlink('test_project', recursive = TRUE)
