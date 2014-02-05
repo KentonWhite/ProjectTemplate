8f707d6b9df0afd7279e6cbea00cbb9c1b52aeb4
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-02-05 12:37:33 +0100

    refactor: extract functions .create.project.new and .copy.project.files

--- R/create.project.R
+++ R/create.project.R
@@ -29,32 +29,9 @@
 #' \dontrun{create.project('MyProject')}
 create.project <- function(project.name = 'new-project', minimal = FALSE, dump = FALSE)
 {
-  tmp.dir <- paste(project.name, '_tmp', sep = '')
+  template.name <- if (minimal) 'minimal' else 'full'
+  .create.project.new(template.name, project.name)
 
-  if (file.exists(project.name) || file.exists(tmp.dir))
-  {
-    stop(paste("Cannot run create.project() from a directory containing", project.name, "or", tmp.dir))
-  }
-
-  dir.create(tmp.dir)
-
-  if (minimal)
-  {
-    file.copy(system.file(file.path('defaults', 'minimal'), package = 'ProjectTemplate'),
-              file.path(tmp.dir),
-              recursive = TRUE)
-    file.rename(file.path(tmp.dir, 'minimal'),
-                project.name)
-  }
-  else
-  {
-    file.copy(system.file(file.path('defaults', 'full'), package = 'ProjectTemplate'),
-              file.path(tmp.dir),
-              recursive = TRUE)
-    file.rename(file.path(tmp.dir, 'full'),
-                project.name)
-  }
-  
   if (dump)
   {
     1; # Magic happens here to place all of the R files from ProjectTemplate in the current folder.
@@ -71,6 +48,25 @@ create.project <- function(project.name = 'new-project', minimal = FALSE, dump =
           file = file.path(project.name, paste(item, '.R', sep = '')))
     }
   }
+}
 
-  unlink(tmp.dir, recursive = TRUE)
+.create.project.new <- function(template.name, project.name) {
+  tmp.dir <- paste(project.name, '_tmp', sep = '')
+
+  if (file.exists(project.name) || file.exists(tmp.dir))
+  {
+    stop(paste("Cannot run create.project() from a directory containing", project.name, "or", tmp.dir))
+  }
+
+  dir.create(tmp.dir)
+  on.exit(unlink(tmp.dir, recursive = TRUE), add=TRUE)
+
+  .copy.project.files(template.name, tmp.dir)
+  file.rename(file.path(tmp.dir, template.name), project.name)
+}
+
+.copy.project.files <- function(template.name, tmp.dir) {
+  file.copy(system.file(file.path('defaults', template.name), package = 'ProjectTemplate'),
+            file.path(tmp.dir),
+            recursive = TRUE)
 }
