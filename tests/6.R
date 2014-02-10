af7acfc7ff0756a363aa07c6a3f6afb68ced6f38
Kenton White <jkentonwhite@gmail.com>
2014-02-10 17:06:06 -0500

    Move tests/5.R to tests/6.R
    Conflict with test file in develop branch

---
+++ tests/6.R
@@ -0,0 +1,30 @@
+library('testthat')
+
+library('ProjectTemplate')
+
+foo1.reader <- function() {}
+	
+.add.extension('foo1', foo1.reader)
+expect_that(ProjectTemplate:::extensions.dispatch.table[['\\.foo1$']], equals(foo1.reader))
+
+create.project('test_project', minimal = FALSE)
+file.copy(file.path(system.file('example_data',
+                                  package = 'ProjectTemplate'),
+                      'foo.reader.R'), file.path('test_project', 'lib', 'foo.reader.R'))
+file.copy(file.path(system.file('example_data',
+                                  package = 'ProjectTemplate'),
+                      'example.foo'), file.path('test_project', 'data', 'example.foo'))
+
+expect_that(file.exists(file.path('test_project', 'lib', 'foo.reader.R')), is_true())
+expect_that(file.exists(file.path('test_project', 'data', 'example.foo')), is_true())
+
+setwd('test_project')
+
+load.project()
+expect_that(ProjectTemplate:::extensions.dispatch.table[['\\.foo$']], equals(foo.reader))
+expect_that(get('example',envir = .GlobalEnv), equals("bar"))
+setwd('..')
+
+unlink('test_project', recursive = TRUE)
+rm(foo1.reader)
+
