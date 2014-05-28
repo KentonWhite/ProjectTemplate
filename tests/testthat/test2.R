dfe349a0eea4b7206ae5c53287edcfd148cd48ba
junkka <johan.junkka@gmail.com>
2014-05-28 19:28:01 +0200

    Migrate tests to test_that structure

--- tests/testthat/test2.R
+++ tests/testthat/test2.R
@@ -1,19 +1,21 @@
-library('testthat')
+context('Clean variable names')
 
-library('ProjectTemplate')
+test_that('Cleans variable names', {
+  
+  create.project('test_project')
 
-create.project('test_project')
+  setwd('test_project')
 
-setwd('test_project')
+  load.project()
 
-load.project()
+  expect_that(ProjectTemplate:::clean.variable.name('test_me'), equals('test.me'))
+  expect_that(ProjectTemplate:::clean.variable.name('test-me'), equals('test.me'))
+  expect_that(ProjectTemplate:::clean.variable.name('test..me'), equals('test.me'))
+  expect_that(ProjectTemplate:::clean.variable.name('test me'), equals('test.me'))
+  expect_that(ProjectTemplate:::clean.variable.name('1990'), equals('X1990'))
 
-expect_that(ProjectTemplate:::clean.variable.name('test_me'), equals('test.me'))
-expect_that(ProjectTemplate:::clean.variable.name('test-me'), equals('test.me'))
-expect_that(ProjectTemplate:::clean.variable.name('test..me'), equals('test.me'))
-expect_that(ProjectTemplate:::clean.variable.name('test me'), equals('test.me'))
-expect_that(ProjectTemplate:::clean.variable.name('1990'), equals('X1990'))
+  setwd('..')
 
-setwd('..')
+  unlink('test_project', recursive = TRUE)
+})
 
-unlink('test_project', recursive = TRUE)
--- tests/testthat/test5.R
+++ tests/testthat/test5.R
@@ -1,145 +1,146 @@
-library('testthat')
+context('With existing project')
 
-library('ProjectTemplate')
+test_that('Test full project into existing directory', {
 
-unlink('test_project', recursive = TRUE)
+  expect_that(file.exists(file.path('test_project')), is_false())
+  dir.create('test_project')
+  expect_that(file.exists(file.path('test_project')), is_true())
 
-# Test full project into existing directory.
+  create.project('test_project', minimal = FALSE)
 
-expect_that(file.exists(file.path('test_project')), is_false())
-dir.create('test_project')
-expect_that(file.exists(file.path('test_project')), is_true())
+  expect_that(file.exists(file.path('test_project', 'cache')), is_true())
+  expect_that(file.exists(file.path('test_project', 'config')), is_true())
+  expect_that(file.exists(file.path('test_project', 'config', 'global.dcf')), is_true())
+  expect_that(file.exists(file.path('test_project', 'data')), is_true())
+  expect_that(file.exists(file.path('test_project', 'diagnostics')), is_true())
+  expect_that(file.exists(file.path('test_project', 'doc')), is_true())
+  expect_that(file.exists(file.path('test_project', 'graphs')), is_true())
+  expect_that(file.exists(file.path('test_project', 'lib')), is_true())
+  expect_that(file.exists(file.path('test_project', 'lib', 'helpers.R')), is_true())
+  expect_that(file.exists(file.path('test_project', 'logs')), is_true())
+  expect_that(file.exists(file.path('test_project', 'munge')), is_true())
+  expect_that(file.exists(file.path('test_project', 'munge', '01-A.R')), is_true())
+  expect_that(file.exists(file.path('test_project', 'profiling')), is_true())
+  expect_that(file.exists(file.path('test_project', 'profiling', '1.R')), is_true())
+  expect_that(file.exists(file.path('test_project', 'reports')), is_true())
+  expect_that(file.exists(file.path('test_project', 'src')), is_true())
+  expect_that(file.exists(file.path('test_project', 'tests')), is_true())
+  expect_that(file.exists(file.path('test_project', 'tests', '1.R')), is_true())
+  expect_that(file.exists(file.path('test_project', 'README')), is_true())
+  expect_that(file.exists(file.path('test_project', 'TODO')), is_true())
 
-create.project('test_project', minimal = FALSE)
+  setwd('test_project')
 
-print(list.files('test_project'))
+  load.project()
+  test.project()
 
-expect_that(file.exists(file.path('test_project', 'cache')), is_true())
-expect_that(file.exists(file.path('test_project', 'config')), is_true())
-expect_that(file.exists(file.path('test_project', 'config', 'global.dcf')), is_true())
-expect_that(file.exists(file.path('test_project', 'data')), is_true())
-expect_that(file.exists(file.path('test_project', 'diagnostics')), is_true())
-expect_that(file.exists(file.path('test_project', 'doc')), is_true())
-expect_that(file.exists(file.path('test_project', 'graphs')), is_true())
-expect_that(file.exists(file.path('test_project', 'lib')), is_true())
-expect_that(file.exists(file.path('test_project', 'lib', 'helpers.R')), is_true())
-expect_that(file.exists(file.path('test_project', 'logs')), is_true())
-expect_that(file.exists(file.path('test_project', 'munge')), is_true())
-expect_that(file.exists(file.path('test_project', 'munge', '01-A.R')), is_true())
-expect_that(file.exists(file.path('test_project', 'profiling')), is_true())
-expect_that(file.exists(file.path('test_project', 'profiling', '1.R')), is_true())
-expect_that(file.exists(file.path('test_project', 'reports')), is_true())
-expect_that(file.exists(file.path('test_project', 'src')), is_true())
-expect_that(file.exists(file.path('test_project', 'tests')), is_true())
-expect_that(file.exists(file.path('test_project', 'tests', '1.R')), is_true())
-expect_that(file.exists(file.path('test_project', 'README')), is_true())
-expect_that(file.exists(file.path('test_project', 'TODO')), is_true())
+  setwd('..')
 
-setwd('test_project')
+  unlink('test_project', recursive = TRUE)
 
-load.project()
-test.project()
+})
 
-setwd('..')
+test_that('Test minimal project into existing directory with an unrelated entry', {
 
-unlink('test_project', recursive = TRUE)
+  expect_that(file.exists(file.path('test_project')), is_false())
+  dir.create('test_project')
+  expect_that(file.exists(file.path('test_project')), is_true())
+  file.create(file.path('test_project', '.dummy'))
+  expect_that(file.exists(file.path('test_project', '.dummy')), is_true())
+  dir.create(file.path('test_project', 'dummy_dir'))
+  expect_that(file.exists(file.path('test_project', 'dummy_dir')), is_true())
 
-# Test minimal project into existing directory with an unrelated entry.
+  create.project('test_project', minimal = TRUE, merge.strategy = "allow.non.conflict")
 
-expect_that(file.exists(file.path('test_project')), is_false())
-dir.create('test_project')
-expect_that(file.exists(file.path('test_project')), is_true())
-file.create(file.path('test_project', '.dummy'))
-expect_that(file.exists(file.path('test_project', '.dummy')), is_true())
-dir.create(file.path('test_project', 'dummy_dir'))
-expect_that(file.exists(file.path('test_project', 'dummy_dir')), is_true())
+  expect_that(file.exists(file.path('test_project', 'cache')), is_true())
+  expect_that(file.exists(file.path('test_project', 'config')), is_true())
+  expect_that(file.exists(file.path('test_project', 'config', 'global.dcf')), is_true())
+  expect_that(file.exists(file.path('test_project', 'data')), is_true())
+  expect_that(file.exists(file.path('test_project', 'munge')), is_true())
+  expect_that(file.exists(file.path('test_project', 'munge', '01-A.R')), is_true())
+  expect_that(file.exists(file.path('test_project', 'src')), is_true())
+  expect_that(file.exists(file.path('test_project', 'README')), is_true())
 
-create.project('test_project', minimal = TRUE, merge.strategy = "allow.non.conflict")
+  setwd('test_project')
 
-expect_that(file.exists(file.path('test_project', 'cache')), is_true())
-expect_that(file.exists(file.path('test_project', 'config')), is_true())
-expect_that(file.exists(file.path('test_project', 'config', 'global.dcf')), is_true())
-expect_that(file.exists(file.path('test_project', 'data')), is_true())
-expect_that(file.exists(file.path('test_project', 'munge')), is_true())
-expect_that(file.exists(file.path('test_project', 'munge', '01-A.R')), is_true())
-expect_that(file.exists(file.path('test_project', 'src')), is_true())
-expect_that(file.exists(file.path('test_project', 'README')), is_true())
+  load.project()
 
-setwd('test_project')
+  setwd('..')
 
-load.project()
+  unlink('test_project', recursive = TRUE)
+})
 
-setwd('..')
+test_that('Test failure creating project into existing directory with an unrelated entry if merge.existing is not set', {
 
-unlink('test_project', recursive = TRUE)
+  expect_that(file.exists(file.path('test_project')), is_false())
+  dir.create('test_project')
+  expect_that(file.exists(file.path('test_project')), is_true())
+  file.create(file.path('test_project', '.dummy'))
+  expect_that(file.exists(file.path('test_project', '.dummy')), is_true())
+  dir.create(file.path('test_project', 'dummy_dir'))
+  expect_that(file.exists(file.path('test_project', 'dummy_dir')), is_true())
 
-# Test failure creating project into existing directory with an unrelated entry
-# if merge.existing is not set.
+  expect_error(create.project('test_project', minimal = TRUE), "not empty")
 
-expect_that(file.exists(file.path('test_project')), is_false())
-dir.create('test_project')
-expect_that(file.exists(file.path('test_project')), is_true())
-file.create(file.path('test_project', '.dummy'))
-expect_that(file.exists(file.path('test_project', '.dummy')), is_true())
-dir.create(file.path('test_project', 'dummy_dir'))
-expect_that(file.exists(file.path('test_project', 'dummy_dir')), is_true())
+  unlink('test_project', recursive = TRUE)
+})
 
-expect_error(create.project('test_project', minimal = TRUE), "not empty")
+test_that('Test failure creating project in directory with existing empty directory matching the name of a template directory', {
 
-unlink('test_project', recursive = TRUE)
+  expect_that(file.exists(file.path('test_project')), is_false())
+  dir.create('test_project')
+  expect_that(file.exists(file.path('test_project')), is_true())
+  dir.create(file.path('test_project', 'munge'))
+  expect_that(file.exists(file.path('test_project', 'munge')), is_true())
 
-# Test failure creating project in directory with existing empty directory
-# matching the name of a template directory
+  expect_error(create.project('test_project', minimal = TRUE,
+                              merge.strategy = "allow.non.conflict"), "overwrite")
 
-expect_that(file.exists(file.path('test_project')), is_false())
-dir.create('test_project')
-expect_that(file.exists(file.path('test_project')), is_true())
-dir.create(file.path('test_project', 'munge'))
-expect_that(file.exists(file.path('test_project', 'munge')), is_true())
+  unlink('test_project', recursive = TRUE)
+  
+})
 
-expect_error(create.project('test_project', minimal = TRUE,
-                            merge.strategy = "allow.non.conflict"), "overwrite")
+test_that('Test failure creating project in directory with existing file matching the name of a template directory',{
 
-unlink('test_project', recursive = TRUE)
+  expect_that(file.exists(file.path('test_project')), is_false())
+  dir.create('test_project')
+  expect_that(file.exists(file.path('test_project')), is_true())
+  file.create(file.path('test_project', 'munge'))
+  expect_that(file.exists(file.path('test_project', 'munge')), is_true())
 
-# Test failure creating project in directory with existing file
-# matching the name of a template directory
+  expect_error(create.project('test_project', minimal = TRUE,
+                              merge.strategy = "allow.non.conflict"), "overwrite")
 
-expect_that(file.exists(file.path('test_project')), is_false())
-dir.create('test_project')
-expect_that(file.exists(file.path('test_project')), is_true())
-file.create(file.path('test_project', 'munge'))
-expect_that(file.exists(file.path('test_project', 'munge')), is_true())
+  unlink('test_project', recursive = TRUE)
 
-expect_error(create.project('test_project', minimal = TRUE,
-                            merge.strategy = "allow.non.conflict"), "overwrite")
+})
 
-unlink('test_project', recursive = TRUE)
+test_that('Test failure creating project in directory with existing empty directory matching the name of a template file', {
 
-# Test failure creating project in directory with existing empty directory
-# matching the name of a template file
+  expect_that(file.exists(file.path('test_project')), is_false())
+  dir.create('test_project')
+  expect_that(file.exists(file.path('test_project')), is_true())
+  dir.create(file.path('test_project', 'README'))
+  expect_that(file.exists(file.path('test_project', 'README')), is_true())
 
-expect_that(file.exists(file.path('test_project')), is_false())
-dir.create('test_project')
-expect_that(file.exists(file.path('test_project')), is_true())
-dir.create(file.path('test_project', 'README'))
-expect_that(file.exists(file.path('test_project', 'README')), is_true())
+  expect_error(create.project('test_project', minimal = TRUE,
+                              merge.strategy = "allow.non.conflict"), "overwrite")
 
-expect_error(create.project('test_project', minimal = TRUE,
-                            merge.strategy = "allow.non.conflict"), "overwrite")
+  unlink('test_project', recursive = TRUE)
+  
+})
 
-unlink('test_project', recursive = TRUE)
+test_that('Test failure creating project in directory with existing file matching the name of a template file', {
 
-# Test failure creating project in directory with existing file
-# matching the name of a template file
+  expect_that(file.exists(file.path('test_project')), is_false())
+  dir.create('test_project')
+  expect_that(file.exists(file.path('test_project')), is_true())
+  file.create(file.path('test_project', 'README'))
+  expect_that(file.exists(file.path('test_project', 'README')), is_true())
 
-expect_that(file.exists(file.path('test_project')), is_false())
-dir.create('test_project')
-expect_that(file.exists(file.path('test_project')), is_true())
-file.create(file.path('test_project', 'README'))
-expect_that(file.exists(file.path('test_project', 'README')), is_true())
+  expect_error(create.project('test_project', minimal = TRUE,
+                              merge.strategy = "allow.non.conflict"), "overwrite")
 
-expect_error(create.project('test_project', minimal = TRUE,
-                            merge.strategy = "allow.non.conflict"), "overwrite")
-
-unlink('test_project', recursive = TRUE)
+  unlink('test_project', recursive = TRUE)
+  
+})
