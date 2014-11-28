46b1a4a0846a93c77f7d0c2bab06fea76880046d
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2014-11-28 12:10:45 +0100

    merge test files to reduce smells

--- tests/testthat/test-create.R
+++ tests/testthat/test-create.R
@@ -91,3 +91,134 @@ test_that('Miminal project', {
   suppressMessages(load.project())
 
 })
+
+test_that('Test full project into existing directory', {
+
+  test_project <- tempfile('test_project')
+  expect_that(file.exists(file.path(test_project)), is_false())
+  dir.create(test_project)
+  on.exit(unlink(test_project, recursive = TRUE), add = TRUE)
+  expect_that(file.exists(file.path(test_project)), is_true())
+
+  suppressMessages(create.project(test_project, minimal = FALSE))
+
+  oldwd <- setwd(test_project)
+  on.exit(setwd(oldwd), add = TRUE)
+
+  expect_full()
+
+  suppressMessages(load.project())
+  suppressMessages(test.project())
+
+})
+
+test_that('Test minimal project into existing directory with an unrelated entry', {
+
+  test_project <- tempfile('test_project')
+  expect_that(file.exists(file.path(test_project)), is_false())
+  dir.create(test_project)
+  on.exit(unlink(test_project, recursive = TRUE), add = TRUE)
+  expect_that(file.exists(file.path(test_project)), is_true())
+
+  suppressMessages(create.project(test_project, minimal = TRUE, merge.strategy = "allow.non.conflict"))
+
+  oldwd <- setwd(test_project)
+  on.exit(setwd(oldwd), add = TRUE)
+
+  expect_minimal()
+
+  suppressMessages(load.project())
+
+})
+
+test_that('Test failure creating project into existing directory with an unrelated entry if merge.existing is not set', {
+
+  test_project <- tempfile('test_project')
+  expect_that(file.exists(file.path(test_project)), is_false())
+  dir.create(test_project)
+  on.exit(unlink(test_project, recursive = TRUE), add = TRUE)
+  expect_that(file.exists(file.path(test_project)), is_true())
+
+  file.create(file.path(test_project, '.dummy'))
+  expect_that(file.exists(file.path(test_project, '.dummy')), is_true())
+  dir.create(file.path(test_project, 'dummy_dir'))
+  expect_that(file.exists(file.path(test_project, 'dummy_dir')), is_true())
+
+  expect_error(
+    suppressMessages(
+      create.project(test_project, minimal = TRUE), "not empty"))
+
+})
+
+test_that('Test failure creating project in directory with existing empty directory matching the name of a template directory', {
+
+  test_project <- tempfile('test_project')
+  expect_that(file.exists(file.path(test_project)), is_false())
+  dir.create(test_project)
+  on.exit(unlink(test_project, recursive = TRUE), add = TRUE)
+  expect_that(file.exists(file.path(test_project)), is_true())
+
+  dir.create(file.path(test_project, 'munge'))
+  expect_that(file.exists(file.path(test_project, 'munge')), is_true())
+
+  expect_error(
+    suppressMessages(
+      create.project(test_project, minimal = TRUE,
+                     merge.strategy = "allow.non.conflict"), "overwrite"))
+
+})
+
+test_that('Test failure creating project in directory with existing file matching the name of a template directory',{
+
+  test_project <- tempfile('test_project')
+  expect_that(file.exists(file.path(test_project)), is_false())
+  dir.create(test_project)
+  on.exit(unlink(test_project, recursive = TRUE), add = TRUE)
+  expect_that(file.exists(file.path(test_project)), is_true())
+
+  file.create(file.path(test_project, 'munge'))
+  expect_that(file.exists(file.path(test_project, 'munge')), is_true())
+
+  expect_error(
+    suppressMessages(
+      create.project(test_project, minimal = TRUE,
+                     merge.strategy = "allow.non.conflict"), "overwrite"))
+
+})
+
+test_that('Test failure creating project in directory with existing empty directory matching the name of a template file', {
+
+  test_project <- tempfile('test_project')
+  expect_that(file.exists(file.path(test_project)), is_false())
+  dir.create(test_project)
+  on.exit(unlink(test_project, recursive = TRUE), add = TRUE)
+  expect_that(file.exists(file.path(test_project)), is_true())
+
+  dir.create(file.path(test_project, 'README.md'))
+  expect_that(file.exists(file.path(test_project, 'README.md')), is_true())
+
+  expect_error(
+    suppressMessages(
+      create.project(test_project, minimal = TRUE,
+                     merge.strategy = "allow.non.conflict"), "overwrite"))
+
+
+})
+
+test_that('Test failure creating project in directory with existing file matching the name of a template file', {
+
+  test_project <- tempfile('test_project')
+  expect_that(file.exists(file.path(test_project)), is_false())
+  dir.create(test_project)
+  on.exit(unlink(test_project, recursive = TRUE), add = TRUE)
+  expect_that(file.exists(file.path(test_project)), is_true())
+
+  file.create(file.path(test_project, 'README.md'))
+  expect_that(file.exists(file.path(test_project, 'README.md')), is_true())
+
+  expect_error(
+    suppressMessages(
+      create.project(test_project, minimal = TRUE,
+                     merge.strategy = "allow.non.conflict"), "overwrite"))
+
+})
