fb206be5309bcf8def059a7e2176a29f3ab65ac2
tomschloss <tom.schlosser@gmail.com>
2016-03-11 08:17:46 +0000

    Improves flexibility of separated.list

--- R/separated.list.R
+++ R/separated.list.R
@@ -1,33 +1,33 @@
 #' Create a separated list where some vars are blank
 #'
-#' This creates a list separated by the specified \code{sep}. It ignores
-#' vars that are blank, which is useful for specifying sql.reader functions
-#' which often create a concatenated string. It means that the sql.reader
-#' functions can be more flexible when reading .sql files.
+#' This creates a list separated by the specified \code{sepchar}. It ignores
+#' vars that are blank, which is useful for specifying \code{\link{sql.reader}}
+#' odbc connections which need a concatenated string separated by a semi-colon.
+#' It means that the sql.reader functions can be more flexible when reading .sql
+#' dcf files, as the user can specify the inputs for \code{\link{odbcDriverConnect}}.
 #'
-#' @param ... these are all the params to be passed to RODBC
-#' @param sepchar this is the value you wish to separate the files with
-#' @param database.info this is a list of the information translated from
-#'  the relevant .dcf file
+#' @param sepchar this is the character with which you wish to separate list.
+#' @param database.info this is a list to be concatenated. It is in the form
+#'  of the information translated from a .dcf file using \code{\link{translate.dcf}}
+#' @param ignore this is an optional char var to ignore particular vars
+#'  in the dcf.
 #'
 #' @examples
 #' library('ProjectTemplate')
 #'
 #' \dontrun{
-#'  a <- "username"
-#'  b <- "password"
-#'  d <- "notinthelist"
-#'  e <- NA
-#'  f <- NULL
+#'  separated.list(sepchar = ";", database.info = database.info, ignore = c("ridiculousoption"))}
 #'
-#'  separated.list(sep = ";", database.info = "database.info", a, b, d)}
+#' @seealso \code{\link{sql.reader}} for useage and \code{\link{translate.dcf}} for
+#' \code{database.info input}
+#'
+#' @include .require.package.R
 #'
 
-separated.list <- function(sepchar = ";", database.info, ...){
+separated.list <- function(sepchar = ";", database.info, ignore){
   # look in the field, if blank return nothing else
   # return string in form: "field=database.info[['field']];"
-  input_list <- list(...)
-  input_list <- unlist(input_list[which(input_list != "")])
+  input_list <- names(database.info)
   output_list <- NULL
 
   for(i in 1:length(input_list)){
