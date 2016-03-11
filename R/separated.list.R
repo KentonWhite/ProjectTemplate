ea426a9eebe9f08ab9c7a639db162bc87735bdf8
tomschloss <tom.schlosser@gmail.com>
2016-03-11 08:58:02 +0000

    Updated the documentation for sql.reader and separated.list. Generated rd files

--- R/separated.list.R
+++ R/separated.list.R
@@ -1,37 +1,43 @@
-#' Create a separated list where some vars are blank
+#' Create a concatenated string from an R list.
 #'
-#' This creates a list separated by the specified \code{sepchar}. It ignores
-#' vars that are blank, which is useful for specifying \code{\link{sql.reader}}
-#' odbc connections which need a concatenated string separated by a semi-colon.
-#' It means that the sql.reader functions can be more flexible when reading .sql
-#' dcf files, as the user can specify the inputs for \code{\link{odbcDriverConnect}}.
+#' This creates a list separated by the specified \code{sepchar}, which is
+#' useful for specifying \code{\link{sql.reader}} odbc connections which
+#' need a concatenated string separated by a semi-colon. It ignores
+#' list elements that are blank.
+#' It means that the \code{sql.reader} functions can be more flexible when
+#' reading .sql dcf files, as the user can specify the inputs for
+#' \code{\link{odbcDriverConnect}}.
 #'
 #' @param sepchar this is the character with which you wish to separate list.
-#' @param database.info this is a list to be concatenated. It is in the form
-#'  of the information translated from a .dcf file using \code{\link{translate.dcf}}
-#' @param ignore this is an optional char var to ignore particular vars
-#'  in the dcf.
+#' @param target.list this is a list to be concatenated. It is in the form
+#'  of the information translated from a .dcf file using
+#'  \code{\link{translate.dcf}}
+#' @param ignore this is an optional character vector to ignore particular
+#'  elements in the list.
 #'
 #' @examples
 #' library('ProjectTemplate')
 #'
 #' \dontrun{
-#'  separated.list(sepchar = ";", database.info = database.info, ignore = c("ridiculousoption"))}
+#'  separated.list(sepchar = ";",
+#'    target.list = database.info,
+#'    ignore = c("ridiculousoption")
+#'    )
+#'    }
 #'
-#' @seealso \code{\link{sql.reader}} for useage and \code{\link{translate.dcf}} for
-#' \code{database.info input}
+#' @seealso \code{\link[ProjectTemplate]{sql.reader}} for useage and
+#' \code{\link[ProjectTemplate]{translate.dcf}} for \code{database.info} form.
 #'
-#' @include .require.package.R
 #'
 
-separated.list <- function(sepchar = ";", database.info, ignore){
+separated.list <- function(sepchar = ";", target.list, ignore){
   # look in the field, if blank return nothing else
-  # return string in form: "field=database.info[['field']];"
-  input_list <- names(database.info)
+  # return string in form: "field=target.list[['field']];"
+  input_list <- names(target.list)
   output_list <- NULL
 
   for(i in 1:length(input_list)){
-    config_param <- database.info[[input_list[i]]]
+    config_param <- target.list[[input_list[i]]]
     if(is.null(config_param)){
       config_param <- NA
     }
