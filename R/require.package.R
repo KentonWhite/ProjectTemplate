c17701b71b932c649f5154dd174827ba21f3c1a6
wilmeragsgh <wilmeragsgm@gmail.com>
2016-06-14 15:21:09 -0400

    fixing duplicing require and using require vs library

--- R/require.package.R
+++ R/require.package.R
@@ -42,9 +42,9 @@ require.package <- function(package.name, attach = TRUE)
     #function.name <- deparse(sys.calls()[[sys.nframe()-1]], nlines = 1)
     message('We load or install the required libraries')
     #stop(paste(function.name, ' requires package ', package.name, '.\nPlease install ', package.name, ' by running install.packages("', package.name, '") and then try re-running load.project()', sep = ''), call. = FALSE)
+    require(package.name,character.only = T)
   }
 
-  library(package.name,character.only = T)
   invisible(NULL)
 }
 
