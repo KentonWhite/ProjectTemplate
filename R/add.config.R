04805dc750801c50559ab7498a5848f7681f7d24
connectedblue <connectedblue@users.noreply.github.com>
2016-09-14 21:07:15 +0100

    Updated documentation for add.config and renamed globals.R skeleton

--- R/add.config.R
+++ R/add.config.R
@@ -1,6 +1,11 @@
 #' Add project specific config to the global config
 #'
-#' Enables project specific configuation to be added to the global config object
+#' Enables project specific configuation to be added to the global config object.  The
+#' allowable format is key value pairs which are appended to the end of the \code{config}
+#' object, which is accessible from the global environment.
+#' 
+#' Once defined, the value can be accessed from any \code{ProjectTemplate} script by
+#' referencing \code{config$my_project_var}.
 #'
 #' @param ... A series of key-value pairs containing the configuration.  The key is the
 #'            name that gets added to the config object.
@@ -16,6 +21,8 @@
 #'     keep_bigdata=TRUE,     # Whether to keep the big data file in memory
 #'     parse=7                # number of fields to parse 
 #' )
+#' 
+#' if (config$keep_bigdata) ...
 #' }
 add.config <- function(...){
         project_config <- list(...)
