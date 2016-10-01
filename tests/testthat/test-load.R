e908329e24add60ada9c350ce573239a0b9fbaeb
connectedblue <connectedblue@users.noreply.github.com>
2016-10-01 12:03:07 +0100

    Added some unit tests to check for error messages when loading and creating projects

--- tests/testthat/test-load.R
+++ tests/testthat/test-load.R
@@ -11,3 +11,15 @@ test_that('All elements have length 1', {
   suppressMessages(load.project())
   expect_equal(unname(vapply(config, length, integer(1))), rep(1L, length(config)))
 })
+
+test_that('Dont load when not in ProjectTemplate directory', {
+        test_project <- tempfile('test_project')
+        dir.create(test_project)
+        on.exit(unlink(test_project, recursive = TRUE), add = TRUE)
+        
+        oldwd <- setwd(test_project)
+        on.exit(setwd(oldwd), add = TRUE)
+        
+        expect_message(load.project(), "is not a ProjectTemplate directory")
+        
+})
