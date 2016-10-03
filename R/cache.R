6ad2e06b93f53f18221209df6923a0c4ae532cd9
connectedblue <connectedblue@users.noreply.github.com>
2016-10-03 22:12:21 +0100

    Faster cache function (#155)
    * Added clear.cache function
    
    * Updated DESCRIPTION with clear.cache in Collate field
    
    * Processed documentation in roxygen
    
    * Try to fix documentation error issues
    
    * Added a modification to the cache function
    
    * modified load.project to read in a custom config file for the project if it exists
    
    * Created README to document changes
    
    * Update readme changes
    
    * Moved global config reader out of load.project and into a new function create.config.  Added a new lib file called globals.R containing a skeleton function call to create.config
    
    * Added an is.cached helper function
    
    * changed create.config to add.config (more natural)
    
    * added clearer user messaging at the end of the function to show which objects were removed and which were unable to be removed
    
    * Updated documentation for add.config and renamed globals.R skeleton
    
    * force globals.R in lib to be read first during load.project
    
    * Updated website documentation for global config.  Also updated README in the lib directory
    
    * added .write.cache function
    
    * added cache hashing function
    
    * added more helper functions to read and write from the cache
    
    * save work
    
    * first working version of cache with new functionality
    
    * cache and clear.cache working as first iteration
    
    * added spaces in front of user messages so they indent nicely and you can see which munge script they refer to
    
    * added better user messaging to inform about when caching is performed
    
    * Explicitly stop cache processing if a supplied variable does not exist
    
    * Add new feature to strip comments from CODE so changes won't be unnessarily re-evalued
    
    * Fixed import declaration of formatR (integration test failed on previous commit)
    
    * Added a cache status message when the function cache is called with no arguments
    
    * changed CODE parameter to accept blocks rather than quotation marks
    
    * changed order of CODE and depends in cache function call to allow for more flexible uses.  Also added website documentation in a new page called caching
    
    * Added several unit tests for the various caching scenarios

--- R/cache.R
+++ R/cache.R
@@ -4,10 +4,23 @@
 #' directory. This cached copy of the data set will then be given precedence
 #' at load time when calling \code{\link{load.project}}. Cached data sets are
 #' stored as \code{.RData} files.
+#' 
+#' Usually you will want to cache datasets during munging.  This can be the raw
+#' data just loaded, or it can be the result of further processing during munge.  Either
+#' way, it can take a while to cache large variables, so cache will only cache when it
+#' needs to.  
+#' The \code{clear.cache("variable")} command
+#' can be run to flush individual items from the cache.
+#' 
+#' Calling \code{cache()} with no arguments returns the current status of the cache.
 #'
-#' @param variable A character vector containing the name of the variable to
-#'  be saved.
-#'
+#' @param variable A character string containing the name of the variable to
+#'  be saved.  If the CODE parameter is defined, it is evaluated and saved, otherwise
+#'  the variable with that name in the global environment is used.
+#' @param CODE A sequence of R statements enclosed in \code{\{..\}} which produce the object to be
+#' cached.  
+#' @param depends A character vector of other global environment objects that the CODE
+#' depends upon. Caching will be forced if those objects have changed since last caching
 #' @param ... additional arguments passed to \code{\link{save}}
 #'
 #' @return No value is returned; this function is called for its side effects.
@@ -24,11 +37,256 @@
 #'
 #' setwd('..')
 #' unlink('tmp-project')}
-cache <- function(variable, ...)
+cache <- function(variable=NULL, CODE=NULL, depends=NULL,  ...)
 {
+  if (is.null(variable)) return(.cache.status())
+        
   stopifnot(length(variable) == 1)
-  save(list = variable,
-       envir = .TargetEnv,
-       file = file.path('cache', paste0(variable, '.RData')),
-       ...)
+  
+  CODE <- paste0(deparse(substitute(CODE)), collapse ="\n")
+  if (CODE=="NULL") CODE <- NULL
+  
+  # strip out comments and newlines from CODE so that changes to those
+  # don't force a re-evaluation of CODE unncessarily.
+  
+  if (!is.null(CODE)){
+          .require.package("formatR")
+          
+          CODE <- formatR::tidy_source(text = CODE, comment = FALSE,
+                                      blank = FALSE, brace.newline = FALSE,
+                                      output = FALSE, width.cutoff = 500)
+          CODE <- paste(CODE$text.tidy, sep="", collapse="\n")
+  }
+  
+  # Check what's already in the cache for variable
+  stored <- .read.cache.info(variable)
+  
+  # The idea is to only cache if you need to, making scripts faster.
+  # 
+  # cache rules:
+  #    - if variable not in cache:
+  #              if CODE==NULL & variable in globenv:
+  #                          save(variable)
+  #                          save(hash(variable))
+  #              if CODE==NULL & variable not in globenv:
+  #                          error:  variable doesn't exist
+  #              Otherwise:
+  #                          save(variable=eval(CODE))
+  #                          save(hash(variable))
+  #                          save(hash(each var in depends))
+  #              
+  #    - if variable in cache:                           
+  #              if CODE==NULL & variable in globenv:
+  #                          if hash(globalenv(variable))==hash(cached(variable)):
+  #                                   msg:  Skipping caching of variable:  up to date
+  #                          Otherwise:
+  #                                   save(variable)
+  #                                   save(hash(variable))
+  #              if CODE==NULL & variable not in globenv:
+  #                          msg:  cached variable not updated
+  #              Otherwise:
+  #                          if hash(eval(CODE))==hash(cached(variable)):
+  #                                   msg:  Skipping caching of variable:  up to date
+  #                          Otherwise:
+  #                                  save(variable=eval(CODE))
+  #                                  save(hash(variable))
+  #                                  save(hash(each var in depends))
+  #  
+  #                    
+  #  Each code block below either exits early with an appropriate message why the
+  #  cache wasn't updated, or the appropriate cache.hash object is created for saving
+  #  at the end.
+  #  See the .write.cache function for a description of the cache.hash object
+  #  which is saved in the cache directory along with the cached variable.
+
+  if (!stored$in.cache) {
+          if (is.null(CODE)){
+                  # genv.hash contains the current hash value from the global env
+                  # which can be compared to the stored value for variable
+                  genv.hash <- .create.cache.hash(variable, NULL, NULL)
+                  if (is.null(genv.hash)) {
+                          message(paste0("  Cannot cache ", variable, 
+                                         ": Does not exist in global environment and no code to create it"))
+                          return(invisible(NULL))
+                  }
+                  message(paste0("  Creating cache entry from global environment: ", variable))
+                  cache.hash <- genv.hash
+          }
+          else {
+                  message(paste0("  Creating cache entry from CODE: ", variable))
+                  .evaluate.code(variable, CODE)
+                  cache.hash <- .create.cache.hash(variable, depends, CODE)
+          }
+  }         
+  else {
+          if (is.null(CODE)) {
+                  genv.hash <- .create.cache.hash(variable, NULL, NULL)
+                  if (is.null(genv.hash)) {
+                          message(paste0("  Unable to update cache for ", variable, 
+                                         ": Does not exist in global environment and no code to create it"))
+                          return(invisible(NULL))
+                  }
+                  if (stored$hash["VAR",]$hash == genv.hash["VAR",]$hash) {
+                          message(paste0("  Skipping cache update for ", variable, 
+                                         ": up to date"))
+                          return(invisible(NULL))
+                  }
+                  message(paste0("  Updating existing cache entry from global environment: ", variable))
+                  cache.hash <- genv.hash
+          }
+          else {
+                  genv.hash <- .create.cache.hash(variable, depends, CODE)
+                  genv.hash <- genv.hash[!grepl("VAR", row.names(genv.hash)),]
+                  stored.hash <- stored$hash[!grepl("VAR", row.names(stored$hash)),]
+                  if (isTRUE(all.equal(genv.hash, stored.hash))) {
+                          message(paste0("  Skipping cache update for ", variable, 
+                                         ": up to date"))
+                          return(invisible(NULL))
+                  }
+                  message(paste0("  Updating existing cache entry from CODE: ", variable))
+                  .evaluate.code(variable, CODE)
+                  cache.hash <- .create.cache.hash(variable, depends, CODE)
+          }
+          
+  }
+
+  # if we end up here then save the variable to the cache
+  .write.cache(cache.hash, ...)
 }
+
+
+.write.cache <- function(cache.hash, ...){
+        # cache.hash is a data frame with two columns:  variable and hash.
+        # Row name VAR is the name of the variable to save.
+        # Row name CODE is the hash value of the code to compute variable.
+        # Row name DEPENDS.* are the dependent variables that CODE depends on.
+        # The helper function .create.cache.hash creates a suitable dataframe
+        
+        variable <- as.character(cache.hash["VAR",]$variable)
+        cache_filename <- .cache.filename(variable)
+        
+        # cache the variable
+        save(list = variable,
+             envir = .TargetEnv,
+             file = cache_filename$obj,
+             ...)
+        
+        # hash information is stored in a separate file to the data is so
+        # it can be retrieved quickly when things need to be read from the cache
+        save(list = "cache.hash",
+             envir = environment(),
+             file = cache_filename$hash
+             )
+}
+
+.create.cache.hash <- function(variable, depends, CODE) {
+        # This function prepares a cache.hash suitable for
+        # saving to the cache
+        # It loops through each of the inputs and computes the hash
+        # using the .cache.hash helper function
+        
+        cache.hash <- .cache.hash(variable)
+        if (is.null(cache.hash)) return(NULL)
+        
+        row.names(cache.hash) <- "VAR"
+        if (!is.null(CODE)){
+                code.hash <- .cache.hash("CODE", environment())
+                row.names(code.hash) <- "CODE"
+                cache.hash <- rbind(cache.hash, code.hash)
+        }
+        if (!is.null(depends)){
+                depends.hash <- .cache.hash(depends)
+                if (nrow(depends.hash)>=1) {
+                        row.names(depends.hash) <- paste0("DEPENDS.", 1:nrow(depends.hash))
+                        cache.hash <- rbind(cache.hash, depends.hash)
+                }
+        }
+        cache.hash
+}
+
+
+.cache.hash <- function (variables, env=.TargetEnv) {
+        # input is a vector of variable names  
+        # check if they exist in the supplied environment
+        # and return a dataframe of their hash values
+        .require.package("digest")
+ 
+        missing <- variables[!sapply(variables, exists, envir=env)]
+        if (length(missing)>0){
+                return(NULL)
+        }
+
+        
+        hashes <- c()
+        for (var in variables) {
+                hashes <- c(hashes, digest::digest(get(var, envir=env)))
+        }
+        
+        data.frame(variable=variables, hash=hashes, stringsAsFactors = FALSE)
+}
+
+
+.read.cache.info <- function (variable) {
+        
+        cache_name <- .cache.filename(variable)
+        
+        in.cache <- FALSE
+        if (file.exists(cache_name$obj)) in.cache <- TRUE
+        
+        hash <- FALSE
+        cache.hash <- NULL
+        if (file.exists(cache_name$hash) & in.cache) {
+                # hash data frame will be loaded into cache.hash
+                load(cache_name$hash, envir = environment())
+        }
+        
+        list(in.cache=in.cache, hash=cache.hash)
+}
+
+.evaluate.code <- function (variable, CODE) {
+        # run code and assignthe results to variable in the global env
+        result <- eval(parse(text=CODE), envir = .TargetEnv)
+        assign(variable, result, envir = .TargetEnv)
+}
+
+.cache.filename <- function(variable) {
+        list(
+                obj=file.path('cache', paste0(variable, '.RData')),
+                hash=file.path('cache', paste0(variable, '.hash'))
+        )
+}
+
+.cache.status <- function () {
+        cached_variables <- .cached.variables()
+        if (length(cached_variables)==0) {
+                return(message("No variables in cache"))
+        }
+        status <- ""
+        for (var in cached_variables) {
+                var_info <- .read.cache.info(var)
+                status <- paste0(status, "Variable: ", var, "\n")
+                if(is.data.frame(var_info$hash)) {
+                        code <- var_info$hash[grepl("CODE", row.names(var_info$hash)),]
+                        depends <- var_info$hash[grepl("DEPENDS", row.names(var_info$hash)),]
+                        if (nrow(code) > 0) {
+                                status <- paste0(status, "  Generated from supplied CODE\n")
+                        }
+                        if (nrow(depends) > 0) {
+                                status <- paste0(status, "    Depends on variable: ", depends$variable, "\n")
+                        }
+                }
+                else {
+                        status <- paste0(status, "  Incomplete cache record.  Recommend running clear.cache('", var, "') and load.project()\n")
+                }
+                
+        }
+        message(status)
+}
+
+.cached.variables <- function(){
+        # get all relevant cache files
+        cache_files <- list.files("cache", pattern="\\.(RData|hash)$")
+        # and return the variable names
+        unique(sub("^(.*)\\..*$", "\\1", cache_files))
+}
+
---
+++ tests/testthat/test-cache.R
@@ -0,0 +1,283 @@
+context('Caching')
+
+tidy_up <- function () {
+        objs <- setdiff(ls(envir = .TargetEnv), "tidy_up")
+        rm(list = objs, envir = .TargetEnv)
+}
+
+
+test_that('caching a variable that doesnt exist fails with correct message', {
+
+  test_project <- tempfile('test_project')
+  suppressMessages(create.project(test_project, minimal = FALSE))
+  on.exit(unlink(test_project, recursive = TRUE), add = TRUE)
+
+  oldwd <- setwd(test_project)
+  on.exit(setwd(oldwd), add = TRUE)
+  
+  var_to_cache <- "xxxx"
+  
+  # make sure it doesn't exist
+  if (exists(var_to_cache, envir = .TargetEnv )) {
+          rm(list=var_to_cache, envir = .TargetEnv)
+  }
+  # try to cache it
+  expect_message(cache(var_to_cache, CODE = NULL, depends = NULL), 
+                 "Does not exist in global environment and no code to create it")
+  tidy_up()
+})
+
+test_that('caching a variable not already in cache caches correctly', {
+        
+        test_project <- tempfile('test_project')
+        suppressMessages(create.project(test_project, minimal = FALSE))
+        on.exit(unlink(test_project, recursive = TRUE), add = TRUE)
+        
+        oldwd <- setwd(test_project)
+        on.exit(setwd(oldwd), add = TRUE)
+        
+        var_to_cache <- "xxxx"
+        test_data <- data.frame(Names=c("a", "b", "c"), Ages=c(20,30,40))
+        assign(var_to_cache, test_data, envir = .TargetEnv)
+        
+        # Create a new cached version
+        expect_message(cache(var_to_cache, CODE = NULL, depends = NULL), 
+                       "Creating cache entry from global environment")
+        
+        # Remove it from Global Environment
+        rm(list=var_to_cache, envir = .TargetEnv)
+        
+        # Load up from cache and check it's the same as what was originally created
+        suppressMessages(load.project())
+        expect_equal(get(var_to_cache, envir = .TargetEnv) , test_data)
+        
+        tidy_up()
+        
+})
+
+test_that('caching a variable created from CODE caches correctly', {
+        
+        test_project <- tempfile('test_project')
+        suppressMessages(create.project(test_project, minimal = FALSE))
+        on.exit(unlink(test_project, recursive = TRUE), add = TRUE)
+        
+        oldwd <- setwd(test_project)
+        on.exit(setwd(oldwd), add = TRUE)
+        
+        var_to_cache <- "xxxx"
+        test_data <- data.frame(Names=c("a", "b", "c"),
+                                Ages=c(200,300,400))
+        
+        
+        # Create a cached version created from CODE
+        expect_message(cache(var_to_cache, depends = NULL, CODE = {
+                                data.frame(Names=c("a", "b", "c"),
+                                           Ages=c(200,300,400))
+                             }), 
+                       "Creating cache entry from CODE")
+        
+        # Remove it from Global Environment (rm will fail if it's not created from CODE)
+        expect_error(rm(list=var_to_cache, envir = .TargetEnv), NA)
+        
+        # Load up from cache and check it's the same as what was originally created
+        suppressMessages(load.project())
+        expect_equal(get(var_to_cache, envir = .TargetEnv) , test_data)
+        
+        tidy_up()
+        
+})
+
+
+test_that('re-caching is skipped when a cached variable hasnt changed', {
+        
+        test_project <- tempfile('test_project')
+        suppressMessages(create.project(test_project, minimal = FALSE))
+        on.exit(unlink(test_project, recursive = TRUE), add = TRUE)
+        
+        oldwd <- setwd(test_project)
+        on.exit(setwd(oldwd), add = TRUE)
+        
+        var_to_cache <- "xxxx"
+        test_data <- data.frame(Names=c("a", "b", "c"), Ages=c(20,30,40))
+        assign(var_to_cache, test_data, envir = .TargetEnv)
+        
+        # Create initial cached version
+        cache(var_to_cache, CODE = NULL, depends = NULL)
+        initial_mtime <- file.info(file.path('cache', paste0(var_to_cache, ".RData")))$mtime
+        
+        # wait two seconds
+        Sys.sleep(2)
+        
+        # Remove it from Global Environment
+        rm(list=var_to_cache, envir = .TargetEnv)
+        
+        # Load up from cache and attempt to re-cache
+        suppressMessages(load.project())
+        expect_message(cache(var_to_cache, CODE = NULL, depends = NULL), 
+                       "Skipping cache update for")
+        
+        # Check that modification time hasn't changed
+        new_mtime <- file.info(file.path('cache', paste0(var_to_cache, ".RData")))$mtime
+        
+        expect_equal(initial_mtime, new_mtime)
+        
+        tidy_up()
+        
+})
+
+test_that('re-caching is done again when a cached variable has changed', {
+        
+        test_project <- tempfile('test_project')
+        suppressMessages(create.project(test_project, minimal = FALSE))
+        on.exit(unlink(test_project, recursive = TRUE), add = TRUE)
+        
+        oldwd <- setwd(test_project)
+        on.exit(setwd(oldwd), add = TRUE)
+        
+        var_to_cache <- "xxxx"
+        test_data <- data.frame(Names=c("a", "b", "c"), Ages=c(20,30,40))
+        assign(var_to_cache, test_data, envir = .TargetEnv)
+        
+        # Create initial cached version
+        cache(var_to_cache, CODE = NULL, depends = NULL)
+        initial_mtime <- file.info(file.path('cache', paste0(var_to_cache, ".RData")))$mtime
+        
+        # wait two seconds
+        Sys.sleep(2)
+        
+        # Remove it from Global Environment
+        rm(list=var_to_cache, envir = .TargetEnv)
+        
+        # Load up from cache 
+        suppressMessages(load.project())
+        
+        # change the variable and attempt to re-cache
+        test_data2 <- data.frame(Names=c("aaa", "b", "c"), Ages=c(20,30,40))
+        assign(var_to_cache, test_data2, envir = .TargetEnv)
+        
+        expect_message(cache(var_to_cache, CODE = NULL, depends = NULL), 
+                       "Updating existing cache entry from global environment")
+        
+        # Check that modification time has changed
+        new_mtime <- file.info(file.path('cache', paste0(var_to_cache, ".RData")))$mtime
+        expect_false(isTRUE(all.equal(initial_mtime, new_mtime)))
+        
+        # Reload and check that re-cached value is different
+        rm(list=var_to_cache, envir = .TargetEnv)
+        suppressMessages(load.project())
+        
+        expect_false(isTRUE(all.equal(get(var_to_cache, envir = .TargetEnv) , test_data)))
+        
+        tidy_up()
+        
+})
+
+test_that('re-caching fails with correct message if cached variable is not in global env', {
+        
+        test_project <- tempfile('test_project')
+        suppressMessages(create.project(test_project, minimal = FALSE))
+        on.exit(unlink(test_project, recursive = TRUE), add = TRUE)
+        
+        oldwd <- setwd(test_project)
+        on.exit(setwd(oldwd), add = TRUE)
+        
+        var_to_cache <- "xxxx"
+        test_data <- data.frame(Names=c("a", "b", "c"), Ages=c(20,30,40))
+        assign(var_to_cache, test_data, envir = .TargetEnv)
+        
+        # Create initial cached version
+        cache(var_to_cache, CODE = NULL, depends = NULL)
+        initial_mtime <- file.info(file.path('cache', paste0(var_to_cache, ".RData")))$mtime
+        
+        # wait two seconds
+        Sys.sleep(2)
+        
+        # Remove it from Global Environment
+        rm(list=var_to_cache, envir = .TargetEnv)
+        
+        # Load up from cache 
+        suppressMessages(load.project())
+        
+        # And remove it from Global Environment again
+        rm(list=var_to_cache, envir = .TargetEnv)
+        
+        expect_message(cache(var_to_cache, CODE = NULL, depends = NULL), 
+                       "Unable to update cache for")
+        
+        # Check that modification time hasn't changed
+        new_mtime <- file.info(file.path('cache', paste0(var_to_cache, ".RData")))$mtime
+        expect_equal(initial_mtime, new_mtime)
+        
+        tidy_up()
+        
+})
+
+test_that('re-caching a variable created from CODE only happens if code changes, not comments or white space', {
+        
+        test_project <- tempfile('test_project')
+        suppressMessages(create.project(test_project, minimal = FALSE))
+        on.exit(unlink(test_project, recursive = TRUE), add = TRUE)
+        
+        oldwd <- setwd(test_project)
+        on.exit(setwd(oldwd), add = TRUE)
+        
+        var_to_cache <- "xxxx"
+        test_data <- data.frame(Names=c("a", "b", "c"),
+                                Ages=c(20000000,300,400))
+        
+        
+        # Create a cached version created from CODE
+        expect_message(cache(var_to_cache, depends = NULL, CODE = {
+                data.frame(Names=c("a", "b", "c"),
+                           Ages=c(200,300,400))
+        }), 
+        "Creating cache entry from CODE")
+        
+        initial_mtime <- file.info(file.path('cache', paste0(var_to_cache, ".RData")))$mtime
+        
+        # wait two seconds
+        Sys.sleep(2)
+        
+        # Remove it from Global Environment (rm will fail if it's not created from CODE)
+        expect_error(rm(list=var_to_cache, envir = .TargetEnv), NA)
+        
+        # Load up from cache again
+        suppressMessages(load.project())
+        
+        # re-cache, this time adding some comments and whitespace, but not changing code
+        # should skip re-caching
+        expect_message(cache(var_to_cache, depends = NULL, CODE = {
+                #  New comments add in
+                data.frame(Names=c("a", "b", "c"),
+                           Ages=c(200,300,400))    # but code remains the same
+                
+                # extra new lines added for good measure
+        }), 
+        "Skipping cache update for ")
+        
+        # Check that modification time hasn't changed
+        new_mtime <- file.info(file.path('cache', paste0(var_to_cache, ".RData")))$mtime
+        
+        expect_equal(initial_mtime, new_mtime)
+        
+        # re-cache agin, keeping the comments and whitespace, but changing the code
+        expect_message(cache(var_to_cache, depends = NULL, CODE = {
+                #  New comments add in
+                data.frame(Names=c("a", "b", "c"),
+                           Ages=c(20000000,300,400))    # but code remains the same
+                
+                # extra new lines added for good measure
+        }), 
+        "Updating existing cache entry from CODE")
+        
+        # Check that modification time has also changed
+        new_mtime <- file.info(file.path('cache', paste0(var_to_cache, ".RData")))$mtime
+        expect_false(isTRUE(all.equal(initial_mtime, new_mtime)))
+        
+        # Finally check that the new code evaluated correctly
+        expect_equal(get(var_to_cache, envir = .TargetEnv) , test_data)
+        
+        tidy_up()
+        
+})
+
