b59f4e13d70e13d2fbb51e9417c21d8f4bc4c63d
Hugo van den Berg <hugo@tbdwebdesign.nl>
2016-12-30 20:20:01 +0100

    Renamed doc to docs and added references to GitHub Pages configuration

--- R/migrate.project.R
+++ R/migrate.project.R
@@ -17,118 +17,127 @@
 #' \dontrun{migrate.project()}
 migrate.project <- function()
 {
-  my.project.info <- list()
-
   message('Migrating project configuration')
 
   # Load the config and look for specific problems in the configuration that
   # should be fixed (e.g. missing files, missing config item)
   # Also flag up if any items need special handling during migration (for example
   # if something other than the default is appropriate for existing projects)
-  
+
   # Initialise migration flags
   config_conflicts <- FALSE
+  directory_conflicts <- FALSE
   config_warnings <- NULL
-  
-  
+
   # Flags stored in environment env
   env <- environment()
-  
+
   # Detect any conflicts with the existing config file once it has been processed
   # during load.project() (flag for now and handle later on)
-  
-  loaded.config <- tryCatch(.load.config(),
-                      warning=function(w) {
-                          # set up some variables to help process the 
-                          # migration warnings later
-                          
-                          assign("config_conflicts", TRUE, envir = env)
-                          assign("config_warnings", w$message, envir = env)
-                          suppressWarnings(.load.config())
-                      })
-          
-  # Detect other migration issues 
-  
-  
+  loaded.config <- tryCatch(
+    .load.config(),
+    warning = function(w) {
+      # set up some variables to help process the
+      # migration warnings later
+
+      assign("config_conflicts", TRUE, envir = env)
+      assign("config_warnings", w$message, envir = env)
+      suppressWarnings(.load.config())
+    }
+  )
+
+  # Detect other migration issues
+  doc_not_renamed <- dir.exists("doc") && !dir.exists("docs")
+  directory_conflicts <- any(doc_not_renamed)
 
   # Exit if everything up to date
-  if ((
-          .check.version(loaded.config, warn.migrate = FALSE) == 0) 
-       && !config_conflicts
-      ) {
-          message("Already up to date.")
-          return(invisible(NULL))
+  if ((.check.version(loaded.config, warn.migrate = FALSE) == 0) &&
+      !config_conflicts && !directory_conflicts
+    ) {
+    message("Already up to date.")
+    return(invisible(NULL))
   }
-  
+
   # Otherwise ....
-  
+
   # Process config conflicts
   if (config_conflicts) {
-          
-          # Tell the user about problems with their old config
-          
-          message(paste0(c(
-                  "Your existing project configuration in globals.dcf does not contain up to",
-                  paste0("date configuration settings in this version ",
-                  .package.version(),
-                  " of ProjectTemplate.  They will"),
-                  "be added automatically during migration, but you should review afterward."
-                  ),
-                  collapse="\n"))
-          
-          if(grepl("missing a configuration file", config_warnings)) {
-                  message(paste0(c(
-                          "You didn't have a config.dcf file.  One has been created",
-                          "for you using default values"
-                          ),
-                  collapse="\n"))
-          }
-          
-         
-          if(grepl("missing the following entries", config_warnings)) {
-                  message(paste0(c(
-                          "Your config.dcf file was missing entries and defaults",
-                          "have been used.  The missing entries are:"
-                          ),
-                  collapse="\n"))
-                  missing <- sub(".*missing the following entries:([^.]*)\\.(.*)$", "\\1", config_warnings)
-                  message(missing)
-          }
-          
-          if(grepl("contains the following unused entries", config_warnings)) {
-                  message(paste0(c(
-                          "Your config.dcf file contained unused entries which have been",
-                          "removed.  The unused entries are:"
-                          ),
-                  collapse="\n"))
-                  unused <- sub(".*contains the following unused entries:([^.]*)\\.(.*)$", "\\1", config_warnings)
-                  message(unused)
-          }
-          
-                    
-          # Specific logic here for new config items that need special migration treatment
-          
-          if(grepl("cache_loaded_data", config_warnings)) {
-                  # switch the setting to FALSE so as to not mess up any existing
-                  # munge script, but warn the user
-                  loaded.config$cache_loaded_data <- FALSE
-                  message(paste0(c(
-                          "\n",
-                          "There is a new config item called cache_loaded_data which auto-caches data",
-                          "after it has been loaded from the data directory.  This has been switched",
-                          "off for this project in case it breaks your scripts.  However you can switch",
-                          "it on manually by editing global.dcf"),
-                          collapse="\n"))
-          }
-          
+    # Tell the user about problems with their old config
+    message(paste0(c(
+      "Your existing project configuration in globals.dcf does not contain up to",
+      paste0("date configuration settings in this version ",
+             .package.version(),
+             " of ProjectTemplate.  They will"),
+      "be added automatically during migration, but you should review afterward."
+    ),
+    collapse = "\n"))
+
+    if (grepl("missing a configuration file", config_warnings)) {
+      message(paste0(c(
+        "You didn't have a config.dcf file.  One has been created",
+        "for you using default values"
+      ),
+      collapse = "\n"))
+    }
+
+    if (grepl("missing the following entries", config_warnings)) {
+      message(paste0(c(
+        "Your config.dcf file was missing entries and defaults",
+        "have been used.  The missing entries are:"
+      ),
+      collapse = "\n"))
+      missing <- sub(".*missing the following entries:([^.]*)\\.(.*)$", "\\1", config_warnings)
+      message(missing)
+    }
+
+    if (grepl("contains the following unused entries", config_warnings)) {
+      message(paste0(c(
+        "Your config.dcf file contained unused entries which have been",
+        "removed.  The unused entries are:"
+      ),
+      collapse = "\n"))
+      unused <- sub(".*contains the following unused entries:([^.]*)\\.(.*)$", "\\1", config_warnings)
+      message(unused)
+    }
+
+    # Specific logic here for new config items that need special migration treatment
+    if (grepl("cache_loaded_data", config_warnings)) {
+      # switch the setting to FALSE so as to not mess up any existing
+      # munge script, but warn the user
+      loaded.config$cache_loaded_data <- FALSE
+      message(paste0(c(
+        "\n",
+        "There is a new config item called cache_loaded_data which auto-caches data",
+        "after it has been loaded from the data directory.  This has been switched",
+        "off for this project in case it breaks your scripts.  However you can switch",
+        "it on manually by editing global.dcf"),
+        collapse = "\n"))
+    }
   }
-  
+
   # Process other migration conflicts
-  
-  
-  
+  if (doc_not_renamed) {
+    message(paste(
+      "",
+      "The doc directory has been renamed to docs for new projects. This was",
+      "done to make it easy use the new option for GitHub Pages to use the docs",
+      "directory as the root for your website.",
+      "The new docs directory has been created in this project, but no files have",
+      "been moved to prevent links from breaking. Please move the files manually",
+      "and remove the old directory to be fully up to date with ProjectTemplate!",
+      "See https://github.com/blog/2289-publishing-with-github-pages-now-as-easy-as-1-2-3",
+      "for more information on how to configure GitHub Pages.",
+      sep = "\n"
+    ))
+    file.copy(system.file(file.path('defaults', 'full', 'docs'),
+                          package = 'ProjectTemplate', mustWork = TRUE),
+              '.',
+              recursive = TRUE,
+              overwrite = FALSE)
+  }
+
   # Finally, save the validated configuration with the updated version number
-  .save.config(loaded.config)   
-  
+  .save.config(loaded.config)
+
 }
 
