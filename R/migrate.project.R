ba584aaa670f8595d88838bdb698c640b807f13b
Hugo van den Berg <hugo@tbdwebdesign.nl>
2017-01-01 13:05:41 +0100

    Clarified migration message for doc to docs, chenged variable names for boolean flags for clarification

--- R/migrate.project.R
+++ R/migrate.project.R
@@ -26,7 +26,8 @@ migrate.project <- function()
 
   # Initialise migration flags
   config_conflicts <- FALSE
-  directory_conflicts <- FALSE
+  version_conflict <- FALSE
+  other_conflicts <- FALSE
   config_warnings <- NULL
 
   # Flags stored in environment env
@@ -45,15 +46,14 @@ migrate.project <- function()
       suppressWarnings(.load.config())
     }
   )
+  version_conflict <- .check.version(loaded.config, warn.migrate = FALSE) != 0
 
   # Detect other migration issues
   doc_not_renamed <- dir.exists("doc") && !dir.exists("docs")
-  directory_conflicts <- any(doc_not_renamed)
+  other_conflicts <- any(doc_not_renamed)
 
   # Exit if everything up to date
-  if ((.check.version(loaded.config, warn.migrate = FALSE) == 0) &&
-      !config_conflicts && !directory_conflicts
-    ) {
+  if (!any(version_conflict, config_conflicts, other_conflicts)) {
     message("Already up to date.")
     return(invisible(NULL))
   }
@@ -63,39 +63,35 @@ migrate.project <- function()
   # Process config conflicts
   if (config_conflicts) {
     # Tell the user about problems with their old config
-    message(paste0(c(
+    message(paste(
       "Your existing project configuration in globals.dcf does not contain up to",
       paste0("date configuration settings in this version ",
              .package.version(),
              " of ProjectTemplate.  They will"),
-      "be added automatically during migration, but you should review afterward."
-    ),
-    collapse = "\n"))
+      "be added automatically during migration, but you should review afterward.",
+      sep = "\n"))
 
     if (grepl("missing a configuration file", config_warnings)) {
-      message(paste0(c(
+      message(paste(
         "You didn't have a config.dcf file.  One has been created",
-        "for you using default values"
-      ),
-      collapse = "\n"))
+        "for you using default values",
+        sep = "\n"))
     }
 
     if (grepl("missing the following entries", config_warnings)) {
-      message(paste0(c(
+      message(paste(
         "Your config.dcf file was missing entries and defaults",
-        "have been used.  The missing entries are:"
-      ),
-      collapse = "\n"))
+        "have been used.  The missing entries are:",
+        sep = "\n"))
       missing <- sub(".*missing the following entries:([^.]*)\\.(.*)$", "\\1", config_warnings)
       message(missing)
     }
 
     if (grepl("contains the following unused entries", config_warnings)) {
-      message(paste0(c(
+      message(paste(
         "Your config.dcf file contained unused entries which have been",
-        "removed.  The unused entries are:"
-      ),
-      collapse = "\n"))
+        "removed.  The unused entries are:",
+        sep = "\n"))
       unused <- sub(".*contains the following unused entries:([^.]*)\\.(.*)$", "\\1", config_warnings)
       message(unused)
     }
@@ -105,28 +101,24 @@ migrate.project <- function()
       # switch the setting to FALSE so as to not mess up any existing
       # munge script, but warn the user
       loaded.config$cache_loaded_data <- FALSE
-      message(paste0(c(
-        "\n",
+      message(paste("",
         "There is a new config item called cache_loaded_data which auto-caches data",
         "after it has been loaded from the data directory.  This has been switched",
         "off for this project in case it breaks your scripts.  However you can switch",
-        "it on manually by editing global.dcf"),
-        collapse = "\n"))
+        "it on manually by editing global.dcf",
+        sep = "\n"))
     }
   }
 
   # Process other migration conflicts
   if (doc_not_renamed) {
-    message(paste(
+    message(paste("",
+      "The doc directory has now been deprecated and ProjectTemplate now uses docs",
+      "instead. This is to ease integration with GitHub Pages (See",
+      "https://github.com/blog/2289-publishing-with-github-pages-now-as-easy-as-1-2-3).",
       "",
-      "The doc directory has been renamed to docs for new projects. This was",
-      "done to make it easy use the new option for GitHub Pages to use the docs",
-      "directory as the root for your website.",
-      "The new docs directory has been created in this project, but no files have",
-      "been moved to prevent links from breaking. Please move the files manually",
-      "and remove the old directory to be fully up to date with ProjectTemplate!",
-      "See https://github.com/blog/2289-publishing-with-github-pages-now-as-easy-as-1-2-3",
-      "for more information on how to configure GitHub Pages.",
+      "A new docs directory has been created in this project, but your existing doc",
+      "directory remains.  Please review and move manually if you wish.",
       sep = "\n"
     ))
     file.copy(system.file(file.path('defaults', 'full', 'docs'),
@@ -138,6 +130,4 @@ migrate.project <- function()
 
   # Finally, save the validated configuration with the updated version number
   .save.config(loaded.config)
-
 }
-
