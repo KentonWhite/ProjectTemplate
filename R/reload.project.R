1fc5bd4a380d3e04dd7f7ca0758d79a258b9ad02
connectedblue <connectedblue@users.noreply.github.com>
2017-04-18 13:12:23 +0100

    Make reload.project() compatible with clear() and clear.cache() (#183)
    * Added reload functionality and tests
    
    * removed .is.cached function and replaced with existing .read.cach.info function
    
    * Removed clear.cache() behaviour from default behaviour
    
    * Reverted back the clear.cache.R file - not needed for this PR
    
    * added .is.cache.empty() test
    
    * checkpoint
    
    * roll back cache.R
    
    * is.cache.empty function

--- R/reload.project.R
+++ R/reload.project.R
@@ -1,12 +1,16 @@
-#' Reload a project from scratch.
+#' Reload or reset a project
 #'
-#' This function will clear the global environment and reload a project
-#' from scratch. This is useful when you've updated your data sets or
-#' changed your preprocessing scripts.
+#' This function will clear the global environment and reload a project.  This is
+#' useful when you've updated your data sets or changed your preprocessing scripts.
+#' Any \code{sticky_variables} configuration parameter in \code{\link{project.config}}
+#' will remain both in memory and (if present) in the cache by default. If the \code{reset} 
+#' parameter is \code{TRUE}, then all variables are cleared from both the global
+#' environment and the cache.
 #'
-#' @param override.config Named list, allows overriding individual configuration
-#'   items.
-#'
+#' @param ... Optional parameters passed to \code{\link{load.project}}
+#' @param reset A boolean value, which if set \code{TRUE} clears the cache and everything
+#'   in the global environment, including any \code{sticky_variables}
+#'   
 #' @return No value is returned; this function is called for its side effects.
 #'
 #' @export
@@ -17,11 +21,20 @@
 #' \dontrun{load.project()
 #'
 #' reload.project()}
-reload.project <- function(override.config = NULL)
+reload.project <- function(..., reset = FALSE)
 {
   
   project_name <- .stopifnotproject("Change to a valid ProjectTemplate directory and run reload.project() again.")
 
-  rm(list = ls(.TargetEnv), pos = .TargetEnv)
-  load.project(override.config = override.config)
+  if (!reset) {
+          if (!.is.cache.empty())
+                  message("Items in cache will not be cleared\nUse reload.project(reset=TRUE) to clear cache also")
+          clear()
+  }
+  else {
+          clear(force = TRUE)
+          clear.cache()
+  }
+  message("Reloading Project ...")
+  load.project(...)
 }
