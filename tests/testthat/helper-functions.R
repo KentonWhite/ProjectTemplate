757a4d8783ba82f0653183dd04a3c0543fc693cd
Gerold Hepp <gisler@users.noreply.github.com>
2025-05-12 18:50:31 +0200

    Refactor qs tests (#320)
    * refactor qs tests
    
    * fix file names
    
    * "enable" qs
    
    * move all tests out of test-qs.R
    
    ---------
    
    Co-authored-by: Kenton White <jkentonwhite@gmail.com>

--- tests/testthat/helper-functions.R
+++
@@ -1,5 +0,0 @@
-# Function to tidy up at the end of tests
-tidy_up <- function() {
-  objs <- setdiff(ls(envir = .TargetEnv), "tidy_up")
-  rm(list = objs, envir = .TargetEnv)
-}
---
+++ tests/testthat/helper-functions_and_data.R
@@ -0,0 +1,22 @@
+# Function to tidy up at the end of tests
+tidy_up <- function() {
+  objs <- ls(envir = .TargetEnv)
+  rm(list = objs, envir = .TargetEnv)
+}
+
+# Determine available cache file formats
+cache_file_formats <- "RData"
+if (requireNamespace("qs", quietly = TRUE)) {
+  cache_file_formats <- c(cache_file_formats, "qs")
+}
+
+# Function to set cache file format
+set_cache_file_format <- function(cache_file_format) {
+  if (cache_file_format != "RData") {
+    config <- .read.config()
+    config$cache_file_format <- cache_file_format
+    .save.config(config)
+  }
+
+  cache_file_format
+}
